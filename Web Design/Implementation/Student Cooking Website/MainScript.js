b31e3d785af416ac819baa2bb3f940ee6fa423a2
William Novak <89736465+WilliamAnthonyNovak@users.noreply.github.com>
2023-01-23 11:41:15 +0000

    Add files via upload

---
+++ Web Design/Implementation/Student Cooking Website/MainScript.js
@@ -0,0 +1,13 @@
+function showlarge(thisGraphic)
+{thisGraphic.style.width='170px';
+thisGraphic.style.height='170px'
+thisGraphic.style.paddingLeft='10px'
+thisGraphic.style.paddingTop='0px'}
+
+function shownormal(thisGraphic)
+{thisGraphic.style.width='140px';
+thisGraphic.style.height='140px'
+thisGraphic.style.paddingLeft='40px'
+thisGraphic.style.paddingTop='30px'}
+
+/* fix image resize moving other images italian.html */
